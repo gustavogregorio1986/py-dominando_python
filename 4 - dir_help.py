d3c9d0f9fc65603e269f85163b96bda22c27a547
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-01-19 15:48:49 -0300

    desenvolvimentod e projeto paar entender como se domina o python, revisando aijnda melhor para evoluir melhor

---
+++ 4 - dir_help.py
@@ -0,0 +1,24 @@
+nome = 'Vinicius Rocha Lima'
+
+print(nome)
+
+print(type(nome))
+
+print(dir(nome))
+
+print(nome.lower())
+
+print(nome.split())
+
+numero = 20
+
+print(dir(numero))
+
+teste = True
+
+print(dir(teste))
+
+help(print)
+
+help(str.split)
+
